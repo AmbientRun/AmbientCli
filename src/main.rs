603d8f69d6f9c6212041dae0860f63ad3efd3187
chaosprint <chaosprint@gmail.com>
2023-09-09 07:36:48 +0200

    ver in toml when ambient new; improve ver format

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use zip::read::ZipArchive;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let options: Vec<&str> = vec!["v0.2.1", "nightly"];
+    let options: Vec<&str> = vec!["0.2.1", "0.3.0-nightly"];
     let mut show_help = false;
     let args: Vec<String> = std::env::args().skip(1).collect();
 
@@ -48,45 +48,42 @@ async fn main() -> anyhow::Result<()> {
         .prompt();
         match ans {
             Ok(choice) => match choice {
-                "v0.2.1" => {
-                    set_version("stable v0.2.1".to_string());
+                "0.2.1" => {
+                    set_version("0.2.1".to_string());
                 }
-                "nightly" => {
+                "0.3.0-nightly" => {
                     println!();
 
                     let d = inquire::DateSelect::new("Nightly version date:")
                         .with_default(chrono::Utc::now().date_naive())
-                        .with_min_date(chrono::NaiveDate::from_ymd_opt(2023, 8, 30).unwrap())
+                        .with_min_date(chrono::NaiveDate::from_ymd_opt(2023, 9, 7).unwrap())
                         .with_max_date(chrono::Utc::now().date_naive()) //.pred_opt()
                         .prompt()
                         .unwrap();
                     println!("You selected nightly version: {}", d.format("%Y-%m-%d"));
                     println!();
 
-                    set_version(format!("nightly {}", d.format("%Y-%m-%d")));
+                    set_version(format!("0.3.0-nightly-{}", d.format("%Y-%m-%d")));
                 }
                 _ => anyhow::bail!("Unsupported version"),
             },
             Err(e) => anyhow::bail!(e),
         }
-    } else if args.get(0) == Some(&"--version".to_string())
-        || args.get(0) == Some(&"-V".to_string())
-    {
-        println!("ambl version: {}", env!("CARGO_PKG_VERSION"));
-        println!(
-            "{}{}Note that this is just the version of the launcher.{}",
-            color::Bg(color::Magenta),
-            color::Fg(color::Yellow),
-            style::Reset
-        );
-        println!(
-            "You can select Ambient runtime version with {}{}`ambl set-default`{}",
-            color::Bg(color::Magenta),
-            color::Fg(color::Yellow),
-            style::Reset
-        );
     } else {
-        let version = if args.contains(&format!("run")) {
+        if args.get(0) == Some(&"--version".to_string()) || args.get(0) == Some(&"-V".to_string()) {
+            println!("ambl version: {}", env!("CARGO_PKG_VERSION"));
+            println!(
+                "You can select Ambient runtime version with {}{}`ambl set-default`{}",
+                style::Bold,
+                color::Fg(color::Yellow),
+                style::Reset
+            );
+        }
+        let version = if args.contains(&"run".to_string())
+            || args.contains(&"deploy".to_string())
+            || args.contains(&"build".to_string())
+        {
+            // read the ambient.toml file
             let current_dir = std::env::current_dir().expect("Failed to get current directory.");
             let ambient_toml_path = current_dir.join("ambient.toml");
             if !ambient_toml_path.exists() {
@@ -100,17 +97,12 @@ async fn main() -> anyhow::Result<()> {
             if version_str.is_none() {
                 get_version()
             } else {
-                let version_str = version_str.unwrap().to_string();
-                let version = semver::Version::parse(&version_str)
-                    .expect("Failed to parse version using semver.");
-
-                if version.pre == semver::Prerelease::EMPTY {
-                    format!(
-                        "stable {}.{}.{}",
-                        version.major, version.minor, version.patch
-                    )
-                } else {
-                    version.pre.replace("nightly-", "nightly ")
+                match version_str {
+                    Some(version_str) => version_str.to_string(),
+                    None => {
+                        eprintln!("Invalid ambient_version in ambient.toml file. Use the default selected version in ambl.");
+                        get_version()
+                    }
                 }
             }
         } else {
@@ -144,34 +136,33 @@ async fn main() -> anyhow::Result<()> {
             );
         }
 
-        let is_stable = version.split(' ').collect::<Vec<&str>>()[0] == "stable";
-        let version = version.split(' ').collect::<Vec<&str>>()[1];
+        if args.get(0) == Some(&"--version".to_string()) || args.get(0) == Some(&"-V".to_string()) {
+            println!(
+                "{}{}{}Local ambient runtime version (should be the same as the selected):{}",
+                style::Bold,
+                style::Underline,
+                color::Fg(color::Blue),
+                style::Reset
+            );
+        }
+
+        let is_stable = !version.contains("nightly");
+
         // check if the version is installed
         let home_dir = dirs::home_dir().expect("Failed to get home directory.");
         let ambient_dir = home_dir.join(".ambient");
-        let runtime_dir = if is_stable {
-            match std::env::consts::OS {
-                "macos" => ambient_dir.join(format!("ambient-{}", version.replace('.', "-"))),
-                "ubuntu" => ambient_dir.join(format!("ambient-{}", version.replace('.', "-"))),
-                _ => anyhow::bail!("Unsupported OS"),
-            }
-        } else {
-            match std::env::consts::OS {
-                "macos" => {
-                    ambient_dir.join(format!("ambient-nightly-{}", version.replace('.', "-")))
-                }
-                "ubuntu" => {
-                    ambient_dir.join(format!("ambient-nightly-{}", version.replace('.', "-")))
-                }
-                _ => anyhow::bail!("Unsupported OS"),
-            }
+        let runtime_dir = match std::env::consts::OS {
+            "macos" => ambient_dir.join(format!("ambient-{}", version.replace('.', "-"))),
+            "ubuntu" => ambient_dir.join(format!("ambient-{}", version.replace('.', "-"))),
+            _ => anyhow::bail!("Unsupported OS"),
         };
 
         if !runtime_dir.exists() || fs::metadata(&runtime_dir)?.len() == 0 {
+            let os = std::env::consts::OS;
             if is_stable {
                 println!("Downloading stable version... This will only happen once.");
                 download(
-                    get_stable_url(version.to_string())?,
+                    get_url(version.to_string(), os.to_string())?,
                     Version::Stable(version.to_string()),
                 )
                 .await?;
@@ -179,7 +170,7 @@ async fn main() -> anyhow::Result<()> {
             } else {
                 println!("Downloading nightly version... This will only happen once.");
                 download(
-                    get_nightly_url(version.to_string())?,
+                    get_url(version.to_string(), os.to_string())?,
                     Version::Nightly(version.to_string()),
                 )
                 .await?;
@@ -197,6 +188,33 @@ async fn main() -> anyhow::Result<()> {
             .expect("Failed to execute command");
 
         let _status = child.wait().expect("Failed to wait on child");
+
+        if args.contains(&"new".to_string()) {
+            let folder = args.get(1).context("Missing folder name")?;
+            let current_dir = std::env::current_dir().expect("Failed to get current directory.");
+            let ambient_toml_path = current_dir.join(format!("{folder}/ambient.toml"));
+            if !ambient_toml_path.exists() {
+                println!("ambient.toml file not found at {:?}", ambient_toml_path);
+            } else {
+                // read the ambient.toml file and add the ambient_version
+                let ambient_toml_content = fs::read_to_string(&ambient_toml_path)
+                    .expect("Failed to read ambient.toml file.");
+                let mut ambient_toml: Value = toml::from_str(&ambient_toml_content)
+                    .expect("Failed to parse ambient.toml file.");
+                // create [ambient_version] under [package]
+                ambient_toml["package"].as_table_mut().unwrap().insert(
+                    "ambient_version".to_string(),
+                    Value::String(version.to_string()),
+                );
+
+                // Serialize the TOML contents
+                let toml_content = toml::to_string_pretty(&ambient_toml)
+                    .expect("Failed to serialize TOML content.");
+                // Write the updated TOML content back to the file
+                fs::write(&ambient_toml_path, toml_content.as_bytes())
+                    .expect("Failed to write to ambient.toml file.");
+            }
+        }
     }
     Ok(())
 }
@@ -209,21 +227,19 @@ fn wget_is_available() -> bool {
 }
 
 /// Get the download URL for a given version and OS
-/// e.g. `get_url("v0.2.1", "macos")`
+/// e.g. `get_url("0.2.1", "macos")`
 pub fn get_url(version: String, os: String) -> anyhow::Result<String> {
+    let v = semver::Version::parse(&version).context("Failed to parse version")?;
     let s = if !version.contains("nightly") {
-        // todo: more robust validation
-        if !version.contains('v') {
-            anyhow::bail!("Invalid version");
-        };
         match os.as_str() {
-            "windows" => format!("https://github.com/AmbientRun/Ambient/releases/download/{}/ambient-x86_64-pc-windows-msvc.zip", version),
-            "macos" => format!("https://github.com/AmbientRun/Ambient/releases/download/{}/ambient-aarch64-apple-darwin.zip", version),
-            "ubuntu" => format!("https://github.com/AmbientRun/Ambient/releases/download/{}/ambient-x86_64-unknown-linux-gnu.zip", version),
+            "windows" => format!("https://github.com/AmbientRun/Ambient/releases/download/v{}.{}.{}/ambient-x86_64-pc-windows-msvc.zip", v.major, v.minor, v.patch),
+            "macos" => format!("https://github.com/AmbientRun/Ambient/releases/download/v{}.{}.{}/ambient-aarch64-apple-darwin.zip", v.major, v.minor, v.patch),
+            "ubuntu" => format!("https://github.com/AmbientRun/Ambient/releases/download/v{}.{}.{}/ambient-x86_64-unknown-linux-gnu.zip", v.major, v.minor, v.patch),
             _ => anyhow::bail!("Unsupported OS"),
         }
     } else {
-        // e.g. nightly-2023-08-31
+        // e.g. 0.3.0.nightly-2023-08-31
+        let version = v.pre;
         let date = version.replace("nightly-", "");
         match os.as_str() {
             "macos" => format!("https://storage.googleapis.com/ambient-artifacts/ambient-nightly-build/{date}/macos-latest/ambient-aarch64-apple-darwin.zip"),
@@ -232,30 +248,8 @@ pub fn get_url(version: String, os: String) -> anyhow::Result<String> {
             _ => anyhow::bail!("Unsupported OS"),
         }
     };
+
     Ok(s)
-}
-
-/// Get the download URL for a given version and OS
-pub fn get_stable_url(version: String) -> anyhow::Result<String> {
-    let url = match std::env::consts::OS {
-        "windows" => format!("https://github.com/AmbientRun/Ambient/releases/download/{}/ambient-x86_64-pc-windows-msvc.zip", version),
-        "macos" => format!(
-            "https://github.com/AmbientRun/Ambient/releases/download/{}/ambient-aarch64-apple-darwin.zip", version),
-        "ubuntu" => format!("https://github.com/AmbientRun/Ambient/releases/download/{}/ambient-x86_64-unknown-linux-gnu.zip", version),
-        _ => anyhow::bail!("Unsupported OS"),
-    };
-    Ok(url)
-}
-
-/// Get the download URL for a given version and OS
-pub fn get_nightly_url(date: String) -> anyhow::Result<String> {
-    let url = match std::env::consts::OS {
-        "macos" => format!("https://storage.googleapis.com/ambient-artifacts/ambient-nightly-build/{date}/macos-latest/ambient-aarch64-apple-darwin.zip"),
-        "ubuntu" => format!("https://storage.googleapis.com/ambient-artifacts/ambient-nightly-build/{date}/ubuntu-22.04/ambient-x86_64-unknown-linux-gnu.zip"),
-        "windows" => format!("https://storage.googleapis.com/ambient-artifacts/ambient-nightly-build/{date}/windows-latest/ambient-x86_64-pc-windows-msvc.zip"),
-        _ => anyhow::bail!("Unsupported OS"),
-    };
-    Ok(url)
 }
 
 /// The version of the runtime
@@ -296,7 +290,7 @@ pub async fn download(url: String, v: Version) -> anyhow::Result<String> {
                 dest_folder.join(format!("ambient-{}", version.replace('.', "-")))
             }
             Version::Nightly(version) => {
-                dest_folder.join(format!("ambient-nightly-{}", version.replace('.', "-")))
+                dest_folder.join(format!("ambient-{}", version.replace('.', "-")))
             }
         };
 
@@ -349,10 +343,7 @@ fn get_version() -> String {
         let mut file =
             std::fs::File::create(&config_path).expect("Failed to create config.toml file.");
         let mut config = toml::value::Table::new();
-        config.insert(
-            "default".to_string(),
-            Value::String("stable v0.2.1".to_string()),
-        );
+        config.insert("default".to_string(), Value::String("0.2.1".to_string()));
 
         let toml_content =
             toml::to_string_pretty(&config).expect("Failed to serialize TOML content.");
@@ -360,7 +351,7 @@ fn get_version() -> String {
         file.write_all(toml_content.as_bytes())
             .expect("Failed to write to config.toml file.");
         println!("config.toml file created at {:?}", config_path);
-        "stable v0.2.1".to_string()
+        "0.2.1".to_string()
     } else {
         let config_content = fs::read_to_string(&config_path).expect("Failed to read config file.");
         let config: Value = toml::from_str(&config_content).expect("Failed to parse config file.");
